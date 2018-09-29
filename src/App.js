0837de7b1f5ccfc61f2190ea0f085ad3071c9df7
Thomas Uster <thomasuster@gmail.com>
2018-09-29 13:05:15 -0700

    Some nme looking navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
+import Navigation from './Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="App-header-content">
-            <img src={logo} className="App-logo" alt="logo" />
-            <a className="App-header-nav">Getting Started</a>
-            <a className="App-header-nav">Showcase</a>
-            <a className="App-header-nav">Demos</a>
-            <a className="App-header-nav">Documentation</a>
-          </div>
-        </header>
-        <p className="App-intro">
+        <Navigation />
+        <p className="App-Content">
           Native Media Engine: A proven backend for native iOS, Android, Windows, Mac and Linux
         </p>
       </div>
---
+++ src/Navigation/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import "./styles.css";
+import logo from './logo.svg';
+
+const styles = theme => ({
+  cssRoot: {
+    margin: 1 * theme.spacing.unit,
+    color: theme.palette.getContrastText('#F05A22'),
+    backgroundColor: '#F05A22',
+    '&:hover': {
+      backgroundColor: '#BF360C',
+    },
+  }
+});
+
+function OutlinedButtons(props) {
+  const { classes } = props;
+  return (
+    <div className="Navigation">
+      <div className="Navigation-Section">
+        <img src={logo} className="Navigation-Section-Logo" alt="logo" />
+        <Button variant="outlined" href="/getting_started" className={classes.cssRoot}>Get Started</Button>
+        <Button variant="outlined" href="/showcase" className={classes.cssRoot}>Showcase</Button>
+        <Button variant="outlined" href="/demos" className={classes.cssRoot}>Demos</Button>
+        <Button variant="outlined" href="/documentation" className={classes.cssRoot}>Documentation</Button>
+      </div>
+      <div className="Navigation-Section">
+        <Button variant="outlined" href="#outlined-buttons" className={classes.cssRoot}>Github</Button>
+      </div>
+    </div>
+  );
+}
+
+OutlinedButtons.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(OutlinedButtons);
